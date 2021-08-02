e0c5f2841b9216c7aca9a7953d38db3836eb1bba
oguz gül <ogzgl95@outlook.com>
2021-08-02 15:25:20 +0300

    Create Message.css for view

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,19 @@
+import { CardContent, Typography, Card } from "@material-ui/core";
 import React from "react";
+import "./Message.css";
 
-function Message(props) {
+function Message({ message, username }) {
+  const isUser = username === message.username;
+
   return (
-    <div>
-      <h2>
-        {props.username}: {props.text}
-      </h2>
+    <div className={`message ${isUser && "message__user"}`}>
+      <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+        <CardContent>
+          <Typography color="white" variant="h5" component="h2">
+            {message.username}: {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
