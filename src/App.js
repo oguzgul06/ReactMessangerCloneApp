cb4874e35e8a01db4bff8e9d907c78e8c682e724
oguz gül <ogzgl95@outlook.com>
2021-08-02 14:56:42 +0300

    Create Message.js for chatting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
 import "./App.css";
+import Message from "./Message";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState(["Hello", "Hı", "What's up"]);
+  const [messages, setMessages] = useState([
+    { username: "mete", text: "hey guys" },
+    { username: "cono", text: "whats up" },
+  ]);
+  const [username, setUserName] = useState("");
+
+  useEffect(() => {
+    setUserName(prompt("Please enter your name"));
+  }, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
   return (
     <div className="App">
       <h1>Hello Programmer of My Messenger Clone App</h1>
-
+      <h2>Welcome {username}</h2>
       <form>
         <FormControl>
           <InputLabel>Enter a message...</InputLabel>
@@ -36,7 +45,7 @@ function App() {
       </form>
 
       {messages.map((message) => (
-        <p>{message}</p>
+        <Message username={message.username} text={message.text} />
       ))}
     </div>
   );
