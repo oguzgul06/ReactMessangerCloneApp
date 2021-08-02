e0ee3f57f2d5f9bbfad3d356c1ab2101365a228d
oguz gül <ogzgl95@outlook.com>
2021-08-02 13:56:45 +0300

    Showing send massege on the display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["Hello", "Hı", "What's up"]);
+
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-     <h1>Hello Programmer of My Messenger Clone App</h1>
+      <h1>Hello Programmer of My Messenger Clone App</h1>
 
-    <input/>
-    <button>Send Message</button>
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={sendMessage}>Send Message</button>
 
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
